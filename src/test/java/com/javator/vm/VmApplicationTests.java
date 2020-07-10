49afd06c7564e0dc8f634ab600389cea672372ff
inayathulla.lavani <inayathulla.khan.lavani@pwc.com>
2020-07-10 17:18:54 +0530

    Initial commit

---
+++ src/test/java/com/javator/vm/VmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javator.vm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
